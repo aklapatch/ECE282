b6790b6e42e882c6cf47e0b52f6e9eaf7b8482a0
U-DESKTOP-F54A5DR\me <sniper96a@gmail.com>
2019-02-27 17:00:27 -0500

    added lab 6

---
+++ Lab06/sample-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#This is a comment line
+
+echo -----------------------------------
+echo Show contents of this directory
+ls #lists contents of this directory
+
+echo -----------------------------------
+echo Show users on this machine
+who
+
+echo -----------------------------------
+echo Who is this user?
+whoami
+
+echo -----------------------------------
+echo What is the current working directory?
+pwd
