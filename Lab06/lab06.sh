57ea6ac60798ee4dc71da462e57404a9950f791e
Aaron Klapatch <klapatchaaron@gmail.com>
2019-02-28 11:03:33 -0500

    Add files via upload

--- Lab06/lab06.sh
+++ Lab06/lab06.sh
@@ -2,20 +2,8 @@
 
 BaseUrl="http://ctan.math.utah.edu/ctan/tex-archive/macros/latex/contrib/acmart/"
 
+infile="acmart.html"
 outfile="lab06-links.txt"
 
 # find 'href', cut out href and everything before it, and the ending > and remove the quotes "
-links=$(grep 'href' acmart.html | sed 's/.*href=//' | cut -d '>' -f 1 | sed 's/"//')
-
-# clear output file
-if [ -f $outfile ]
-then
-	rm $outfile
-fi
-
-for link in $links
-do
-# concatenate base url and the further extension and output to the file
-echo "$BaseUrl$link" | sed 's/"//' >> $outfile
-
-done
+grep 'href' $infile | sed 's/.*href=//' | cut -d '>' -f 1 | sed 's|"|'$BaseUrl'|' | sed 's/"//' > $outfile
