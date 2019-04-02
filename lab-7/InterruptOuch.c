8086d86f25aa8b431b838af85a7247300baea1e1
Aaron Klapatch <sniper96a@gmail.com>
2019-04-02 12:36:06 -0400

    got interrupt ouch to perform to specs in canvas

--- lab-7/InterruptOuch.c
+++ lab-7/InterruptOuch.c
@@ -9,6 +9,7 @@
 #include	<stdio.h>
 #include	<signal.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int OuchNum = 0;
 
@@ -17,7 +18,7 @@ void inthandler(int);
 // convert the argument to an int, then return it.
 // return -1 if the arg does not have an int.
 int getIntFromSTR(char * input){
-	if (isdigit(input)){
+	if (isdigit(input[0])){
 		return atoi(input);
 	
 	} else {
@@ -32,29 +33,28 @@ int main(int ac, char *av[]){
 	// parse first argument
 	if (ac > 1) {
 		OuchThreshold = getIntFromSTR(av[1]);
-	
+		if ( OuchThreshold == -1){
+			// argument one is not an integer
+			printf("Argument 1 needs to be an integer.\n");
+			return 1;
+		}
 	} else {
-		printf("\nPlease specify the number of interrupts to tolerate.\n");
+		printf("Usage: %s InterruptThreshold\n", av[0]);
+		printf("Please specify the maximum number of interrupts.\n");
 		return 1;
 	}
 
-	signal( SIGINT,  inthandler );		/* set handler */
+	signal( SIGINT, inthandler );		/* set handler */
 	signal( SIGQUIT, inthandler );		/* set handler */
 
-	printf("\nPress q then enter at any time to quit.\n");
-
-	char UserIn = '\n';
-	do {
-		 UserIn = getchar();
-	} 
-	while( UserIn != 'q' && OuchNum < OuchThreshold );
+	while( OuchNum < OuchThreshold ){}
 }
 
 // increments the interrupt counter and prints the appropriate number of 
 //exclamation points for the number of interrupts recorded
 void inthandler(int s){
 	OuchNum += 1;
-	printf("\nOOch!");
+	printf("\nOOCH!");
 	
 	int i = OuchNum;
 	while(--i) {
